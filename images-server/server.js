1fb5d199a007067fd5f9ff2703e4d69280d272ed
Mike DiGioia <digioia.mike@gmail.com>
2016-03-30 23:16:45 -0400

    Added files via upload
    Allow for recursive lookup

--- images-server/server.js
+++ images-server/server.js
@@ -1,18 +1,20 @@
 var fs = require('fs')
-var destination = "/Users/mdigioia/Desktop/images"
+var read = require('fs-readdir-recursive')
+var destination = "/Users/mdigioia/Desktop/images/"
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/all-images', function(req, res){
-  var filepaths = fs.readdirSync(destination)
+  var filepaths = read(destination)
+
   res.json(filepaths.filter(function(file){
-    return file.match(/\.jpg/);
+    return file.match(/\.(jpg|jpeg|png)$/i)
   }));
 });
 
-app.get('/:imageName', function(req, res){
-  var filename = destination + "/" + req.params.imageName + ".jpg";
+app.get('/:imagepath*', function(req, res){
+  var filename = destination + "/" + req.params.imagepath + req.params[0];
   res.sendFile(filename);
 });
 
